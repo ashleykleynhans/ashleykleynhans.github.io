7e58cddeac0d88a90603f5d36328bc390bcf2a59
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2026-07-07 20:40:35 +0200

    Update documentation

---
+++ theme/dark-theme/dark-theme.js
@@ -0,0 +1,38 @@
+function ThemeManager(options) {
+  var defaultTheme = options.defaultTheme || 'dark';
+  var enableAutoDetectTheme = options.enableAutoDetectTheme.toLowerCase() === 'true';
+  var lightThemeMatch = window.matchMedia('(prefers-color-scheme: light)');
+
+  function applyTheme() {
+    var theme = localStorage.getItem('themeOverride');
+    if (theme !== 'light' && theme !== 'dark') {
+      if (theme === 'browser' || enableAutoDetectTheme) {
+        theme = lightThemeMatch.matches ? 'light' : 'dark';
+      } else {
+        theme = defaultTheme;
+      }
+    }
+
+    if (theme === 'light') {
+      document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');
+    } else {
+      document.body.classList.add('dark-theme');
+      document.body.classList.remove('light-theme');
+    }
+  }
+
+  this.switch = function(themeOverride) {
+    localStorage.setItem('themeOverride', themeOverride);
+    applyTheme();
+    // Reload Disqus to pick up new theme
+    if (typeof DISQUS !== 'undefined') {
+      DISQUS.reset({ reload: true });
+    }
+  };
+
+  applyTheme();
+  lightThemeMatch.addEventListener('change', applyTheme);
+}
+
+window.theme = new ThemeManager(document.getElementById('dark-theme-script').dataset);
